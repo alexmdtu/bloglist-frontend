63bde500a3fc9d165d331429c0e8a11b058d732b
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-13 10:39:32 +0200

    Added styling to blogs.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,16 @@
 import React from 'react'
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 10,
+  paddingBottom: 10,
+  border: 'solid',
+  borderWidth: 1,
+  marginTop: 10,
+  marginBottom: 5
+}
+
 const Blog = ({ blog }) => (
-  <div>
+  <div style={blogStyle}>
     {blog.title} {blog.author}
   </div>
 )
