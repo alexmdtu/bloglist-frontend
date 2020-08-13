70a30a07f26a93c789885ee476e6c17cb0830146
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-13 10:55:22 +0200

    5.7: Added showing and hiding of blog details.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 const blogStyle = {
   paddingTop: 10,
   paddingLeft: 10,
@@ -9,10 +10,32 @@ const blogStyle = {
   marginBottom: 5
 }
 
-const Blog = ({ blog }) => (
-  <div style={blogStyle}>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} by {blog.author}
+        <button onClick={toggleVisibility}>{visible ? 'hide' : 'show'}</button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>
+          Likes: {blog.likes}
+          <button>like</button>
+        </div>
+        <div>Added by: {blog.user.name}</div>
+      </div>
+
+    </div>
+  )
+}
 
 export default Blog
