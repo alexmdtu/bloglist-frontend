d2b10368ff5441bd666deca7ddb57d06014dca6c
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-14 10:42:18 +0200

    5.15: added test to check like button and fixed issue with rendering component for each test beforehand.
    Component needs different props depending on the test which makes rendering it before all not suitable.

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -16,15 +16,10 @@ describe('<Blog />', () => {
     user: dummyUser
   }
 
-  let component
-
-  beforeEach(() => {
-    component = render(
+  test('renders only title and author by default', () => {
+    const component = render(
       <Blog blog={blog} user={dummyUser} />
     )
-  })
-
-  test('renders only title and author by default', () => {
     const hiddenContentDiv = component.container.querySelector('.extraBlogContent')
 
     expect(component.container).toHaveTextContent('Blogtitle by Alex')
@@ -32,10 +27,27 @@ describe('<Blog />', () => {
   })
 
   test('shows blog url and likes after clicking show button', () => {
+    const component = render(
+      <Blog blog={blog} user={dummyUser} />
+    )
     const button = component.getByText('show')
     fireEvent.click(button)
 
     const hiddenContentDiv = component.container.querySelector('.extraBlogContent')
     expect(hiddenContentDiv).not.toHaveStyle('display: none')
+  })
+
+  test('handles like button correctly', () => {
+    const addLike = jest.fn()
+
+    const component = render(
+      <Blog blog={blog} user={dummyUser} likeBlog={addLike} />
+    )
+
+    const button = component.getByText('like', { selector: 'button' })
+    fireEvent.click(button)
+    fireEvent.click(button)
+
+    expect(addLike.mock.calls).toHaveLength(2)
   })
 })
