929d33f538204b5151476a2352103b432c09c545
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-14 10:35:01 +0200

    5.14: added test to check if show buttons work to unhide extra blog content.

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,29 +1,41 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-  test('renders only title and author by default', () => {
-    const dummyUser = {
-      username: 'Alex'
-    }
+  const dummyUser = {
+    username: 'Alex'
+  }
 
-    const blog = {
-      title: 'Blogtitle',
-      author: 'Alex',
-      url: 'www.test.com',
-      likes: 10,
-      user: dummyUser
-    }
+  const blog = {
+    title: 'Blogtitle',
+    author: 'Alex',
+    url: 'www.test.com',
+    likes: 10,
+    user: dummyUser
+  }
 
-    const component = render(
+  let component
+
+  beforeEach(() => {
+    component = render(
       <Blog blog={blog} user={dummyUser} />
     )
+  })
 
+  test('renders only title and author by default', () => {
     const hiddenContentDiv = component.container.querySelector('.extraBlogContent')
 
     expect(component.container).toHaveTextContent('Blogtitle by Alex')
     expect(hiddenContentDiv).toHaveStyle('display: none')
+  })
+
+  test('shows blog url and likes after clicking show button', () => {
+    const button = component.getByText('show')
+    fireEvent.click(button)
+
+    const hiddenContentDiv = component.container.querySelector('.extraBlogContent')
+    expect(hiddenContentDiv).not.toHaveStyle('display: none')
   })
 })
